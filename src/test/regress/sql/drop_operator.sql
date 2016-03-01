0ae5e66dafe4b26b0c698a1f2a1c91dce6854a07
Roma Sokolov <sokolov.r.v@gmail.com>
2016-03-01 18:57:28 +0300

    [fix] simplify tests

--- src/test/regress/sql/drop_operator.sql
+++ src/test/regress/sql/drop_operator.sql
@@ -1,44 +1,6 @@
 CREATE OPERATOR === (
         PROCEDURE = int8eq,
         LEFTARG = bigint,
-        RIGHTARG = bigint
-);
-
-SELECT pg_describe_object(refclassid,refobjid,refobjsubid) as ref, deptype
-FROM pg_depend
-WHERE classid = 'pg_operator'::regclass AND
-      objid = '===(bigint,bigint)'::regoperator
-ORDER BY 1;
-
-CREATE OPERATOR !== (
-        PROCEDURE = int8ne,
-        LEFTARG = bigint,
-        RIGHTARG = bigint,
-        NEGATOR = ===
-);
-
-SELECT po.oprname AS op, po2.oprname AS negator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprnegate
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype;
-
-DROP OPERATOR !== (bigint, bigint);
-
-SELECT po.oprname AS op, po2.oprname AS negator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprnegate
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype;
-
-SELECT oprname, oprnegate FROM pg_operator WHERE oprname = '==='
-    AND oprleft = 'bigint'::regtype AND oprright = 'bigint'::regtype;
-
-DROP OPERATOR ===(bigint,bigint);
-
-CREATE OPERATOR === (
-        PROCEDURE = int8eq,
-        LEFTARG = bigint,
         RIGHTARG = bigint,
         COMMUTATOR = ===
 );
@@ -51,32 +13,17 @@ CREATE OPERATOR !== (
         COMMUTATOR = !==
 );
 
-SELECT po.oprname AS op, po2.oprname AS negator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprnegate
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype;
-
-SELECT po.oprname AS op, po2.oprname AS commutator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprcom
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype
-    ORDER BY po.oprname;
-
 DROP OPERATOR !==(bigint, bigint);
 
-SELECT po.oprname AS op, po2.oprname AS negator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprnegate
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype;
+SELECT	ctid, oprcom
+FROM	pg_catalog.pg_operator fk
+WHERE	oprcom != 0 AND
+	NOT EXISTS(SELECT 1 FROM pg_catalog.pg_operator pk WHERE pk.oid = fk.oprcom);
 
-SELECT po.oprname AS op, po2.oprname AS commutator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprcom
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype;
+SELECT	ctid, oprnegate
+FROM	pg_catalog.pg_operator fk
+WHERE	oprnegate != 0 AND
+	NOT EXISTS(SELECT 1 FROM pg_catalog.pg_operator pk WHERE pk.oid = fk.oprnegate);
 
 DROP OPERATOR ===(bigint, bigint);
 
@@ -94,20 +41,16 @@ CREATE OPERATOR !== (
         COMMUTATOR = ===
 );
 
-SELECT po.oprname AS op, po2.oprname AS commutator, po3.oprname as negator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprcom
-    JOIN pg_operator AS po3 ON po.oid = po3.oprnegate
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprname = '===' AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype;
-
 DROP OPERATOR !==(bigint, bigint);
 
-SELECT po.oprname AS op, po2.oprname AS commutator, po3.oprname as negator FROM pg_operator AS po
-    JOIN pg_operator AS po2 ON po.oid = po2.oprcom
-    JOIN pg_operator AS po3 ON po.oid = po3.oprnegate
-WHERE po.oprnamespace = (SELECT oid FROM
-        pg_namespace WHERE nspname = 'public')
-    AND po.oprname = '===' AND po.oprleft = 'bigint'::regtype AND po.oprright = 'bigint'::regtype;
+SELECT	ctid, oprcom
+FROM	pg_catalog.pg_operator fk
+WHERE	oprcom != 0 AND
+	NOT EXISTS(SELECT 1 FROM pg_catalog.pg_operator pk WHERE pk.oid = fk.oprcom);
+
+SELECT	ctid, oprnegate
+FROM	pg_catalog.pg_operator fk
+WHERE	oprnegate != 0 AND
+	NOT EXISTS(SELECT 1 FROM pg_catalog.pg_operator pk WHERE pk.oid = fk.oprnegate);
 
 DROP OPERATOR ===(bigint, bigint);
